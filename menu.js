485b84d20ff9851e303082fc5c09783a3a49bb08
pam1978 <ranvirkaurbassi@gmail.com>
2026-07-21 11:10:12 +0100

    Add initial menu structure with food items

---
+++ menu.js
@@ -0,0 +1,26 @@
+const menu = {
+
+  fish: [
+    { name: "Mini Cod", price: 4.85 },
+    { name: "Large Cod", price: 9.50 },
+    { name: "Haddock", price: 9.50 },
+    { name: "Fishcake", price: 2.65 },
+    { name: "Scampi", price: 3.65 },
+    { name: "Calamari Strips (6)", price: 3.65 }
+  ],
+
+  burgers: [
+    { name: "Cheeseburger", price: 4.95 },
+    { name: "Chicken Burger", price: 4.95 },
+    { name: "Veggie Burger", price: 4.95 }
+  ],
+
+  kebabs: [],
+  chicken: [],
+  pies: [],
+  extras: [],
+  desserts: [],
+  glutenfree: [],
+  vegan: []
+
+};
